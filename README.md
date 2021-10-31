9b48a54c2ac8f733e148a504f79d7cbe16ffff1c
NMurtata <mrtnbhr@gmail.com>
2021-11-01 00:18:01 +0900

    update

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # OpenCv-Tutorial
 
-commit | feat
+file name | text
 --- |---
 gui | http://labs.eecs.tottori-u.ac.jp/sd/Member/oyamada/OpenCV/html/py_tutorials/py_gui/py_trackbar/py_trackbar.html#trackbar
 canny | http://labs.eecs.tottori-u.ac.jp/sd/Member/oyamada/OpenCV/html/py_tutorials/py_imgproc/py_canny/py_canny.html#canny
--- gui/main.py
+++ gui/main.py
@@ -1,15 +1,17 @@
 import cv2
 import numpy as np
 
+cv2.__version__
+
 def nothing(x):
     pass
 
 img = np.zeros((300, 512, 3), np.uint8)
 cv2.namedWindow("image")
 
-cv2.createTrackbar('R', 'image', 0, 255, nothing)
-cv2.createTrackbar('G', 'image', 0, 255, nothing)
 cv2.createTrackbar('B', 'image', 0, 255, nothing)
+cv2.createTrackbar('G', 'image', 0, 255, nothing)
+cv2.createTrackbar('R', 'image', 0, 255, nothing)
 
 switch = '0:Off\n1 : ON'
 cv2.createTrackbar(switch, 'image', 0, 1, nothing)
